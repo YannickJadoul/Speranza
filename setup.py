71010292af7dae53c1e02b3de12a9c41721c8123
Robin Jadoul <robin.jadoul@gmail.com>
2017-09-07 16:53:06 +0200

    cibuildwheel with c++

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, Extension
 
 setup(
     name="spam",
-    ext_modules=[Extension('spam', sources=['spam.c'])],
+    ext_modules=[Extension('spam', sources=['spam.cpp'], language="c++")],
     version="0.1.0",
 )
